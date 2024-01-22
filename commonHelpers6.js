e8daa2aa9902c48b8db2c1d6858e199a607bc86d
krava101 <141029431+krava101@users.noreply.github.com>
2024-01-22 13:42:30 +0000

    Deploying to gh-pages from @ krava101/CreateX-project@89d05674dfb9f3d7291dcaeada630bc4e2bbf222 🚀

--- commonHelpers6.js
+++ commonHelpers6.js
@@ -1,10 +1,29 @@
-import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                     */const a=[{name:"Cubes Building",type:"Business Centers",location:"Wilhelminakade 663, 3072 AP Rotterdam",client:"Mark Beids",architect:"OMA",size:"162000 sq. m.",value:"$900 million",completed:"June 2013",description:"Completed in 2013, Cubes Building provides spaces for offices, housing, shops and hotels, creating a dynamic urban landscape on the banks of the Meuse River. Its modern and innovative design reflects the modern architectural aesthetic and plays an important role in Rotterdam's modern urban appearance.",images:[]},{name:"Modern Cottage",type:"Private houses",location:"2464 Royal Ln. Mesa, New Jersey",client:"Darlene Robertson",architect:"HIK Architecture",size:"840 sq. feet",value:"$2 million",completed:"May 2020",description:"Build a private house 840 sq. feet with a large living room, three bedrooms, two bathrooms, a terrace, a pool and a garage for two cars. <br/><br/> Modern design and care for each family member to feel as comfortable as possible in the new home.",images:[]}],o=document.querySelector(".hero"),t=window.location.href,l=t[t.length-1];function r(s){const e=a[s],i=`<div class="container">
+import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                     */const c=[{name:"Cubes Building",type:"Business Centers",location:"Wilhelminakade 663, 3072 AP Rotterdam",client:"Mark Beids",architect:"OMA",size:"162000 sq. m.",value:"$900 million",completed:"June 2013",description:"Completed in 2013, Cubes Building provides spaces for offices, housing, shops and hotels, creating a dynamic urban landscapeon the banks of the Meuse River. Its modern and innovative design reflects the modern architectural aesthetic and plays an important rolein Rotterdam's modern urban appearance.",images:["./img/project-img/1-project-1.jpg","./img/project-img/1-project-2.jpg","./img/project-img/1-project-3.jpg","./img/project-img/1-project-4.jpg","./img/project-img/1-project-5.jpg","./img/project-img/1-project-6.jpg"]},{name:"Modern Cottage",type:"Private houses",location:"2464 Royal Ln. Mesa, New Jersey",client:"Darlene Robertson",architect:"HIK Architecture",size:"840 sq. feet",value:"$2 million",completed:"May 2020",description:"Build a private house 840 sq. feet with a large living room, three bedrooms, two bathrooms, a terrace, a pool and a garage for two cars. <br/><br/> Modern design and care for each family member to feel as comfortable as possible in the new home.",images:["./img/project-img/2-project-1.jpg","./img/project-img/2-project-2.jpg","./img/project-img/2-project-3.jpg","./img/project-img/2-project-4.jpg","./img/project-img/2-project-5.jpg","./img/project-img/2-project-6.jpg"]},{name:"Luxury Beach House",type:"Private houses",location:"1226 SW  39th St, Pelican, Cape Coral, Florida",client:"Brad Garley",architect:"MDA",size:"280 sq. m.",value:"$1.7 million",completed:"June 2015",description:"The villa is located on the beautiful Abaco Rose Garden of Cape Coral on a very wide canal and has a fantastic view. This large house is built on a corner lot and has oversized south facing position. 2015 has been completely remodeled and refurbished. <br/> 5 bedrooms and 3 bathrooms with over 3000sq ft has plenty of room for up to 10 people.",images:["./img/project-img/3-project-1.jpg","./img/project-img/3-project-2.jpg","./img/project-img/3-project-3.jpg","./img/project-img/3-project-4.jpg","./img/project-img/3-project-5.jpg","./img/project-img/3-project-6.jpg"]}],n=document.querySelector(".hero"),o=window.location.href,g=o[o.length-1];function m(t){const e=c[t],s=`<div class="container">
           <p class="page-path">
             <span class="page-path-dark">Homepage / Work</span> / ${e.name}
           </p>
           <h1 class="page-title">${e.name}</h1>
           <div class="project-img-list-wrapper">
-            <ul class="project-img-list list"></ul>
+            <ul class="project-img-list list">
+              <li
+                style="background-image: url(${e.images[0]})"
+              ></li>
+              <li
+                style="background-image: url(${e.images[1]})"
+              ></li>
+              <li
+                style="background-image: url(${e.images[2]})"
+              ></li>
+              <li
+                style="background-image: url(${e.images[3]})"
+              ></li>
+              <li
+                style="background-image: url(${e.images[4]})"
+              ></li>
+              <li
+                style="background-image: url(${e.images[5]})"
+              ></li>
+            </ul>
             <button class="slider-left-btn" type="button">
               <svg class="slider-left-icon">
                 <use href="./img/icons.svg#icon-left-arrow"></use>
@@ -17,12 +36,12 @@ import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                
             </button>
           </div>
           <ul class="project-small-img-list list">
-            <li class="project-small-img-item active"></li>
-            <li class="project-small-img-item"></li>
-            <li class="project-small-img-item"></li>
-            <li class="project-small-img-item"></li>
-            <li class="project-small-img-item"></li>
-            <li class="project-small-img-item"></li>
+            <li class="project-small-img-item active" style="background-image: url(${e.images[0]})"></li>
+            <li class="project-small-img-item" style="background-image: url(${e.images[1]})"></li>
+            <li class="project-small-img-item" style="background-image: url(${e.images[2]})"></li>
+            <li class="project-small-img-item" style="background-image: url(${e.images[3]})"></li>
+            <li class="project-small-img-item" style="background-image: url(${e.images[4]})"></li>
+            <li class="project-small-img-item" style="background-image: url(${e.images[5]})"></li>
           </ul>
           <div class="project-info-box">
             <div class="project-goal">
@@ -31,27 +50,19 @@ import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                
                 ${e.description}
               </p>
             </div>
-            <table class="project-info-table">
-              <thead>
-                <tr>
-                  <th>Location</th>
-                  <th>CLIENT</th>
-                  <th>ARCHITECT</th>
-                  <th>SIZE</th>
-                  <th>VALUE</th>
-                  <th>COMPLETED</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>${e.location}</td>
-                  <td>${e.client}</td>
-                  <td>${e.architect}</td>
-                  <td>${e.size}</td>
-                  <td>${e.value}</td>
-                  <td>${e.completed}</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>`;o.innerHTML=i}r(l);
+            <ul class="project-info-list list">
+              <li>Location</li>
+              <li>${e.location}</li>
+              <li>CLIENT</li>
+              <li>${e.client}</li>
+              <li>ARCHITECT</li>
+              <li>${e.architect}</li>
+              <li>SIZE</li>
+              <li>${e.size}</li>
+              <li>VALUE</li>
+              <li>${e.value}</li>
+              <li>COMPLETED</li>
+              <li>${e.completed}</li>
+            </ul>
+          </div>`;n.innerHTML=s}m(g);const p=document.querySelector(".slider-left-btn"),d=document.querySelector(".slider-right-btn"),l=document.querySelector(".project-img-list"),r=document.querySelector(".project-small-img-list");let i=0;p.addEventListener("click",()=>{i<=0?i=l.children.length-1:i--,a(i)});d.addEventListener("click",()=>{i===l.children.length-1?i=0:i++,a(i)});function a(t){Array.from(r.children).map(e=>e.classList.remove("active")),l.style.transform=`translateX(-${i*1230}px)`,r.children[t].classList.add("active")}
 //# sourceMappingURL=commonHelpers6.js.map
