49051707363a7fd986abee4607d1aa577666521c
krava101 <141029431+krava101@users.noreply.github.com>
2024-02-13 00:16:30 +0000

    Deploying to gh-pages from @ krava101/CreateX-project@a3af7fbef1877082dfc48cd9326760613acd9b89 🚀

--- commonHelpers6.js
+++ commonHelpers6.js
@@ -1,80 +1,80 @@
-import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                     */const p=[{id:0,name:"Cubes Building",type:"Business Centers",status:"construction",location:"Wilhelminakade 663, 3072 AP Rotterdam",client:"Mark Beids",architect:"OMA",size:"162000 sq. m.",value:"$900 million",completed:"June 2013",description:"Completed in 2013, Cubes Building provides spaces for offices, housing, shops and hotels, creating a dynamic urban landscapeon the banks of the Meuse River. Its modern and innovative design reflects the modern architectural aesthetic and plays an important rolein Rotterdam's modern urban appearance.",previewImg:new URL("/CreateX-project/assets/project-1-68d98b07.jpg",self.location).href,images:[`${new URL("/CreateX-project/assets/1-project-1-f6b48cbf.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/1-project-2-9f85f9ce.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/1-project-3-34d50af2.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/1-project-4-8e647f7e.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/1-project-5-8ebc5868.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/1-project-6-c763ff4a.jpg",self.location).href}`]},{id:1,name:"Modern Cottage",type:"Private houses",status:"development",location:"2464 Royal Ln. Mesa, New Jersey",client:"Darlene Robertson",architect:"HIK Architecture",size:"840 sq. feet",value:"$2 million",completed:"May 2020",description:"Build a private house 840 sq. feet with a large living room, three bedrooms, two bathrooms, a terrace, a pool and a garage for two cars. <br/><br/> Modern design and care for each family member to feel as comfortable as possible in the new home.",previewImg:new URL("/CreateX-project/assets/project-2-999359c8.jpg",self.location).href,images:[`${new URL("/CreateX-project/assets/2-project-1-941399db.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/2-project-2-ab764684.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/2-project-3-5924128f.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/2-project-4-3cd11c69.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/2-project-5-c2af5df3.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/2-project-6-1987dbb1.jpg",self.location).href}`]},{id:2,name:"Luxury Beach House",type:"Private houses",status:"repairs",location:"1226 SW  39th St, Pelican, Cape Coral, Florida",client:"Brad Garley",architect:"MDA",size:"280 sq. m.",value:"$1.7 million",completed:"June 2015",description:"The villa is located on the beautiful Abaco Rose Garden of Cape Coral on a very wide canal and has a fantastic view. This large house is built on a corner lot and has oversized south facing position. 2015 has been completely remodeled and refurbished. <br/> 5 bedrooms and 3 bathrooms with over 3000sq ft has plenty of room for up to 10 people.",previewImg:new URL("/CreateX-project/assets/project-3-43960424.jpg",self.location).href,images:[`${new URL("/CreateX-project/assets/3-project-1-00fdc6b0.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/3-project-2-64e9013c.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/3-project-3-84a8d952.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/3-project-4-f1f9682b.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/3-project-5-6c5bad9d.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/3-project-6-e43d19dc.jpg",self.location).href}`]},{id:3,name:"Modern Double Bedrooms",type:"Apartments & flats",status:"repairs",location:"121 N 2nd St, Philadelphia, Pennsylvania",client:"Alan Greed",architect:"Loft",size:"20 sq. m.",value:"$50 thousand",completed:"August 2021",description:"The modern double bedroom spanning 20 square meters is a cozy and stylish space designed for maximum comfort and functionality. The bright and contemporary design of this bedroom aims to highlight modern trends in interior decor.",previewImg:new URL("/CreateX-project/assets/project-4-3b4cb380.jpg",self.location).href,images:[`${new URL("/CreateX-project/assets/4-project-1-a57f1507.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/4-project-2-435f1bbe.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/4-project-3-78195891.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/4-project-4-e1f35fe6.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/4-project-5-ac87158d.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/4-project-6-abc0cb3c.jpg",self.location).href}`]},{id:4,name:"Kids Bedroom With Decorations",type:"Apartments & flats",status:"design",location:"1550 Burnett St, Houston, Texas",client:"Elsa Down",architect:"Loft",size:"18 sq. m.",value:"$30 thousand",completed:"May 2022",description:"The kids' bedroom with decorations spanning 18 square meters is a vibrant and imaginative space designed to inspire creativity and provide a comfortable haven for little ones. The decor is carefully curated to create a lively and playful atmosphere.",previewImg:new URL("/CreateX-project/assets/project-5-dd9c9c6a.jpg",self.location).href,images:[`${new URL("/CreateX-project/assets/5-project-1-308ce8f4.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/5-project-2-2987f955.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/5-project-3-9d273bd9.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/5-project-4-61693a19.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/5-project-5-221ca5d6.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/5-project-6-f38b373e.jpg",self.location).href}`]},{id:5,name:"The Pencil Building",type:"Stores & Malls",status:"construction",location:"600 Peachtree Street NE Atlanta, Georgia, United States",client:"CP Group",architect:"CPA",size:"1,312,980 sq ft",value:"$150 million",completed:"October 1992",description:"Bank of America Plaza is a supertall skyscraper between Midtown Atlanta and Downtown Atlanta. At 311.8 m (1,023 ft), as of February 2024 the tower is the 23rd tallest building in the United States, the tallest building in the Southeastern region of the United States, and the tallest building in any U.S. state capital, overtaking the 250 m (820 ft), 50-story One Atlantic Center in height, which held the record as Georgia's tallest building.",previewImg:new URL("/CreateX-project/assets/project-6-5d8ae179.jpg",self.location).href,images:[`${new URL("/CreateX-project/assets/6-project-1-1b4d442f.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/6-project-2-7112c723.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/6-project-3-d38401aa.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/6-project-4-bc262a32.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/6-project-5-d0dc8634.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/6-project-6-86acac11.jpg",self.location).href}`]},{id:6,name:"Red Building",type:"Business Centers",status:"construction",location:"1501 Rue De Bleury, Ville-marie Quebec H3a 0h3",client:"Groupe TEQ",architect:"CS Design",size:"27.850 sqm",value:"$50 million",completed:"2019",description:'Ilot Balmoral, developed by Societe d’Habitation de Montréal (SHDM) and designed by Provencher Roy, a 13-floor mixed-use office building is part of the "Quartier des Spectacles" located in Ilot Balmoral, Montréal. Ilot Balmoral is home to National Film Board (NFB) head office explicitly designed for its filmmaking activities.',previewImg:new URL("/CreateX-project/assets/project-7-0aa03d83.jpg",self.location).href,images:[`${new URL("/CreateX-project/assets/7-project-1-9324d7e4.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/7-project-2-be7bc794.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/7-project-3-c4fda4cb.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/7-project-4-00278fb9.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/7-project-5-bd7e1d24.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/7-project-6-953efaf5.jpg",self.location).href}`]},{id:7,name:"Scandinavian Style Interior",type:"Private houses",status:"design",location:"203 Thomas More Street, Wapping, E1W 1YD, England, United Kingdom",client:"CoTG Group",architect:"Scandinavian",size:"27 sqm",value:"$50 thousand",completed:"2022",description:"Scandinavian style interior design is a popular and distinctive aesthetic that emanates from the Nordic countries - Denmark, Norway, Sweden, Finland, and Iceland. Known for its simplicity, functionality, and emphasis on natural elements, the Scandinavian style has gained global appeal for creating cozy and inviting spaces.",previewImg:new URL("/CreateX-project/assets/project-8-130fc28a.jpg",self.location).href,images:[`${new URL("/CreateX-project/assets/8-project-1-81dd683a.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/8-project-2-6d703f42.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/8-project-3-505a87b7.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/8-project-4-57298484.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/8-project-5-dcaa3fc8.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/8-project-6-c2f17fa7.jpg",self.location).href}`]},{id:8,name:"Brown-Gray House",type:"Private houses",status:"development",location:"203 Thomas More Street, Wapping, E1W 1YD, England, United Kingdom",client:"Cyle Moon",architect:"Scandinavian",size:"160 sqm",value:"$1.7 million",completed:"2021",description:"Scandinavian style is a popular and distinctive aesthetic that emanates from the Nordic countries - Denmark, Norway, Sweden, Finland, and Iceland. Known for its simplicity, functionality, and emphasis on natural elements, the Scandinavian style has gained global appeal for creating cozy and inviting spaces.",previewImg:new URL("/CreateX-project/assets/project-9-78689934.jpg",self.location).href,images:[`${new URL("/CreateX-project/assets/9-project-1-915b9b5c.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/9-project-2-2dffca72.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/9-project-3-6b4db4b1.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/9-project-4-ce3bccb6.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/9-project-5-1eabae2b.jpg",self.location).href}`,`${new URL("/CreateX-project/assets/9-project-6-bed80398.jpg",self.location).href}`]}],j=document.querySelector(".hero"),i=document.querySelector(".project-list"),g=document.querySelector(".projects-title-container"),m=document.querySelector(".projects-info"),c=window.location.href,h=c[c.length-1],l=new URL("/CreateX-project/assets/icons-0db76ac5.svg",self.location).href;function u(a){const e=p[a],s=`<div class="container">
+import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                     */import{p as m}from"./assets/data-projects-98539dd2.js";const p=document.querySelector(".hero"),c=document.querySelector(".project-list"),d=document.querySelector(".projects-title-container"),j=document.querySelector(".projects-info"),a=window.location.href,y=a[a.length-1],o=new URL("/CreateX-project/assets/icons-0db76ac5.svg",self.location).href;function h(t){const l=m[t],i=`<div class="container">
           <p class="page-path">
-            <span class="page-path-dark">Homepage / Work</span> / ${e.name}
+            <span class="page-path-dark">Homepage / Work</span> / ${l.name}
           </p>
-          <h1 class="page-title">${e.name}</h1>
+          <h1 class="page-title">${l.name}</h1>
           <div class="project-img-list-wrapper">
             <ul class="project-img-list list">
               <li
-                style="background-image: url(${e.images[0]})"
+                style="background-image: url(${l.images[0]})"
               ></li>
               <li
-                style="background-image: url(${e.images[1]})"
+                style="background-image: url(${l.images[1]})"
               ></li>
               <li
-                style="background-image: url(${e.images[2]})"
+                style="background-image: url(${l.images[2]})"
               ></li>
               <li
-                style="background-image: url(${e.images[3]})"
+                style="background-image: url(${l.images[3]})"
               ></li>
               <li
-                style="background-image: url(${e.images[4]})"
+                style="background-image: url(${l.images[4]})"
               ></li>
               <li
-                style="background-image: url(${e.images[5]})"
+                style="background-image: url(${l.images[5]})"
               ></li>
             </ul>
             <button class="slider-left-btn" type="button">
               <svg class="slider-left-icon">
-                <use href=${l+"#icon-left-arrow"}></use>
+                <use href=${o+"#icon-left-arrow"}></use>
               </svg>
             </button>
             <button class="slider-right-btn" type="button">
               <svg class="slider-right-icon">
-                <use href=${l+"#icon-right-arrow"}></use>
+                <use href=${o+"#icon-right-arrow"}></use>
               </svg>
             </button>
           </div>
           <ul class="project-small-img-list list">
-            <li class="project-small-img-item active" style="background-image: url(${e.images[0]})"></li>
-            <li class="project-small-img-item" style="background-image: url(${e.images[1]})"></li>
-            <li class="project-small-img-item" style="background-image: url(${e.images[2]})"></li>
-            <li class="project-small-img-item" style="background-image: url(${e.images[3]})"></li>
-            <li class="project-small-img-item" style="background-image: url(${e.images[4]})"></li>
-            <li class="project-small-img-item" style="background-image: url(${e.images[5]})"></li>
+            <li class="project-small-img-item active" style="background-image: url(${l.images[0]})"></li>
+            <li class="project-small-img-item" style="background-image: url(${l.images[1]})"></li>
+            <li class="project-small-img-item" style="background-image: url(${l.images[2]})"></li>
+            <li class="project-small-img-item" style="background-image: url(${l.images[3]})"></li>
+            <li class="project-small-img-item" style="background-image: url(${l.images[4]})"></li>
+            <li class="project-small-img-item" style="background-image: url(${l.images[5]})"></li>
           </ul>
           <div class="project-info-box">
             <div class="project-goal">
               <h2 class="section-title">Project goal</h2>
               <p class="project-goal-text">
-                ${e.description}
+                ${l.description}
               </p>
             </div>
             <ul class="project-info-list list">
               <li>Location</li>
-              <li>${e.location}</li>
+              <li>${l.location}</li>
               <li>CLIENT</li>
-              <li>${e.client}</li>
+              <li>${l.client}</li>
               <li>ARCHITECT</li>
-              <li>${e.architect}</li>
+              <li>${l.architect}</li>
               <li>SIZE</li>
-              <li>${e.size}</li>
+              <li>${l.size}</li>
               <li>VALUE</li>
-              <li>${e.value}</li>
+              <li>${l.value}</li>
               <li>COMPLETED</li>
-              <li>${e.completed}</li>
+              <li>${l.completed}</li>
             </ul>
-          </div>`;j.innerHTML=s,b(e)}u(h);function b(a){const e=p.filter(s=>s.type===a.type&&s!=a);if(e.length){const s=e.reduce((f,o)=>f+`
+          </div>`;p.innerHTML=i,$(l)}h(y);function $(t){const l=m.filter(i=>i.type===t.type&&i!=t);if(l.length){const i=l.reduce((u,s)=>u+`
     <li class="project-card">
       <img
         class="project-img"
-        src="${o.previewImg}"
+        src="${s.previewImg}"
         alt=""
       />
       <div class="project-card-box">
-        <h5 class="card-title">${o.name}</h5>
-        <p class="project-card-text">${o.type}</p>
-        <a class="project-card-link" href="project.html?project=project${o.id}">view project</a>
+        <h5 class="card-title">${s.name}</h5>
+        <p class="project-card-text">${s.type}</p>
+        <a class="project-card-link" href="project.html?project=project${s.id}">view project</a>
       </div>
-    </li>`,"");i.innerHTML=s}else i.innerHTML="<li></li>",g.textContent="",m.style.margin="0"}const w=document.querySelector(".slider-left-btn"),$=document.querySelector(".slider-right-btn"),r=document.querySelector(".project-img-list"),n=document.querySelector(".project-small-img-list");let t=0;w.addEventListener("click",()=>{t<=0?t=r.children.length-1:t--,d(t)});$.addEventListener("click",()=>{t===r.children.length-1?t=0:t++,d(t)});function d(a){Array.from(n.children).map(e=>e.classList.remove("active")),r.style.transform=`translateX(-${t*1230}px)`,n.children[a].classList.add("active")}
+    </li>`,"");c.innerHTML=i}else c.innerHTML="<li></li>",d.textContent="",j.style.margin="0"}const b=document.querySelector(".slider-left-btn"),f=document.querySelector(".slider-right-btn"),r=document.querySelector(".project-img-list"),n=document.querySelector(".project-small-img-list");let e=0;b.addEventListener("click",()=>{e<=0?e=r.children.length-1:e--,g(e)});f.addEventListener("click",()=>{e===r.children.length-1?e=0:e++,g(e)});function g(t){Array.from(n.children).map(l=>l.classList.remove("active")),r.style.transform=`translateX(-${e*1230}px)`,n.children[t].classList.add("active")}
 //# sourceMappingURL=commonHelpers6.js.map
