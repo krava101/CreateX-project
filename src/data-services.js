7b9c4d16775581525e90a3ec0c20488d42f3d7b7
krava <kravalex2004@gmail.com>
2024-02-17 23:13:13 +0200

    Created Service-page.html

---
+++ src/data-services.js
@@ -0,0 +1,26 @@
+export const services = [
+  {
+    id: 0,
+    type: 'Construction',
+    previewImg: new URL('/img/services-page-img/construction.jpg', import.meta.url).href,
+    offers: ["Interior design of apartments", "Interior design of private houses"]
+  },
+  {
+    id: 1,
+    type: 'Project Development',
+    previewImg: new URL('/img/services-page-img/project-development.jpg', import.meta.url).href,
+    offers: ["Interior design of apartments", "Interior design of private houses"]
+  },
+  {
+    id: 2,
+    type: 'Interior Design',
+    previewImg: new URL('/img/services-page-img/interior-design.jpg', import.meta.url).href,
+    offers: ["Interior design of apartments", "Interior design of private houses"]
+  },
+  {
+    id: 3,
+    type: 'Repairs',
+    previewImg: new URL('/img/services-page-img/repairs.jpg', import.meta.url).href,
+    offers: ["Interior design of apartments", "Interior design of private houses"]
+  }
+]
---
+++ src/service-page.js
@@ -0,0 +1,62 @@
+import { services } from "./data-services";
+
+const offerSection = document.querySelector(".offer");
+const href = window.location.href;
+const projectNum = href[href.length - 1];
+const iconUrl = new URL('/img/icons.svg', import.meta.url).href;
+
+function loadService(projectNum){
+  const currentService = services[projectNum];
+  const markup = `
+    <div class="container">
+          <img
+            class="offer-img"
+            src="${currentService.previewImg}"
+            alt="${currentService.type}"
+          />
+          <div class="offer-info">
+            <h2 class="section-title">We offer</h2>
+            <ul class="offer-list list">
+              ${currentService.offers.reduce((acc, e) => {
+                return acc + `<li>
+                <h4 class="offer-list-title">
+                  <svg class="offer-list-icon">
+                    <use href=${iconUrl + '#icon-plus'}></use>
+                  </svg>
+                  ${e}
+                </h4>
+                <p class="offer-list-about">
+                  Adipiscing nunc arcu enim elit mattis eu placerat proin.
+                  Imperdiet elementum faucibus dignissim purus. Fusce parturient
+                  diam magna ullamcorper morbi semper massa ac facilisis.
+                </p>
+              </li>`
+              }, "")}
+            </ul>
+          </div>
+        </div>
+  `
+  offerSection.innerHTML = markup;
+}
+
+loadService(projectNum);
+
+
+const offersList = document.querySelector(".offer-list");
+
+
+offersList.addEventListener("click", event => {
+  if (event.target.nodeName === "LI") {
+    event.target.classList.toggle("visible");
+    if (event.target.classList.contains("visible")) {
+      event.target.querySelector(":scope use").href.baseVal = iconUrl + '#icon-minus';
+    } else {
+      setTimeout(() => {
+        event.target.querySelector(":scope use").href.baseVal = iconUrl + '#icon-plus';
+      },400)
+      
+    }
+  
+  }
+})
+
