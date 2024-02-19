41cb22575d8cab7477e95d542d876cb6a03c148b
krava101 <141029431+krava101@users.noreply.github.com>
2024-02-19 22:25:17 +0000

    Deploying to gh-pages from @ krava101/CreateX-project@2faabcdce5b846a310a5aeeceb5fc1675a44018c 🚀

--- commonHelpers6.js
+++ commonHelpers6.js
@@ -1,70 +1,70 @@
-import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                     */import{p as m}from"./assets/data-projects-98539dd2.js";const p=document.querySelector(".hero"),c=document.querySelector(".project-list"),d=document.querySelector(".projects-title-container"),j=document.querySelector(".projects-info"),o=window.location.href,y=o[o.length-1],a=new URL("/CreateX-project/assets/icons-1e66910e.svg",self.location).href;function h(t){const e=m[t],i=`<div class="container">
+import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                     */import{p as m}from"./assets/data-projects-98539dd2.js";const p=document.querySelector(".hero"),c=document.querySelector(".project-list"),d=document.querySelector(".projects-title-container"),j=document.querySelector(".projects-info"),a=window.location.href,y=a[a.length-1],o=new URL("/CreateX-project/assets/icons-ea889f47.svg",self.location).href;function h(t){const l=m[t],i=`<div class="container">
           <p class="page-path">
-            <span class="page-path-dark">Homepage / Work</span> / ${e.name}
+            <span class="page-path-dark">Homepage / Work</span> / ${l.name}
           </p>
-          <h1 class="page-title">${e.name}</h1>
+          <h1 class="page-title">${l.name}</h1>
           <div class="project-img-list-wrapper">
             <ul class="project-img-list list">
               <li
-                style="background-image: url(${e.images[0]})"
+                style="background-image: url(${l.images[0]})"
               ></li>
               <li
-                style="background-image: url(${e.images[1]})"
+                style="background-image: url(${l.images[1]})"
               ></li>
               <li
-                style="background-image: url(${e.images[2]})"
+                style="background-image: url(${l.images[2]})"
               ></li>
               <li
-                style="background-image: url(${e.images[3]})"
+                style="background-image: url(${l.images[3]})"
               ></li>
               <li
-                style="background-image: url(${e.images[4]})"
+                style="background-image: url(${l.images[4]})"
               ></li>
               <li
-                style="background-image: url(${e.images[5]})"
+                style="background-image: url(${l.images[5]})"
               ></li>
             </ul>
             <button class="slider-left-btn" type="button">
               <svg class="slider-left-icon">
-                <use href=${a+"#icon-left-arrow"}></use>
+                <use href=${o+"#icon-left-arrow"}></use>
               </svg>
             </button>
             <button class="slider-right-btn" type="button">
               <svg class="slider-right-icon">
-                <use href=${a+"#icon-right-arrow"}></use>
+                <use href=${o+"#icon-right-arrow"}></use>
               </svg>
             </button>
           </div>
           <ul class="project-small-img-list list">
-            <li class="project-small-img-item active" style="background-image: url(${e.images[0]})"></li>
-            <li class="project-small-img-item" style="background-image: url(${e.images[1]})"></li>
-            <li class="project-small-img-item" style="background-image: url(${e.images[2]})"></li>
-            <li class="project-small-img-item" style="background-image: url(${e.images[3]})"></li>
-            <li class="project-small-img-item" style="background-image: url(${e.images[4]})"></li>
-            <li class="project-small-img-item" style="background-image: url(${e.images[5]})"></li>
+            <li class="project-small-img-item active" style="background-image: url(${l.images[0]})"></li>
+            <li class="project-small-img-item" style="background-image: url(${l.images[1]})"></li>
+            <li class="project-small-img-item" style="background-image: url(${l.images[2]})"></li>
+            <li class="project-small-img-item" style="background-image: url(${l.images[3]})"></li>
+            <li class="project-small-img-item" style="background-image: url(${l.images[4]})"></li>
+            <li class="project-small-img-item" style="background-image: url(${l.images[5]})"></li>
           </ul>
           <div class="project-info-box">
             <div class="project-goal">
               <h2 class="section-title">Project goal</h2>
               <p class="project-goal-text">
-                ${e.description}
+                ${l.description}
               </p>
             </div>
             <ul class="project-info-list list">
               <li>Location</li>
-              <li>${e.location}</li>
+              <li>${l.location}</li>
               <li>CLIENT</li>
-              <li>${e.client}</li>
+              <li>${l.client}</li>
               <li>ARCHITECT</li>
-              <li>${e.architect}</li>
+              <li>${l.architect}</li>
               <li>SIZE</li>
-              <li>${e.size}</li>
+              <li>${l.size}</li>
               <li>VALUE</li>
-              <li>${e.value}</li>
+              <li>${l.value}</li>
               <li>COMPLETED</li>
-              <li>${e.completed}</li>
+              <li>${l.completed}</li>
             </ul>
-          </div>`;p.innerHTML=i,$(e)}h(y);function $(t){const e=m.filter(i=>i.type===t.type&&i!=t);if(e.length){const i=e.reduce((u,s)=>u+`
+          </div>`;p.innerHTML=i,$(l)}h(y);function $(t){const l=m.filter(i=>i.type===t.type&&i!=t);if(l.length){const i=l.reduce((u,s)=>u+`
     <li class="project-card">
       <img
         class="project-img"
@@ -76,5 +76,5 @@ import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                
         <p class="project-card-text">${s.type}</p>
         <a class="project-card-link" href="project.html?project=project${s.id}">view project</a>
       </div>
-    </li>`,"");c.innerHTML=i}else c.innerHTML="<li></li>",d.textContent="",j.style.margin="0"}const b=document.querySelector(".slider-left-btn"),f=document.querySelector(".slider-right-btn"),r=document.querySelector(".project-img-list"),n=document.querySelector(".project-small-img-list");let l=0;b.addEventListener("click",()=>{l<=0?l=r.children.length-1:l--,g(l)});f.addEventListener("click",()=>{l===r.children.length-1?l=0:l++,g(l)});function g(t){Array.from(n.children).map(e=>e.classList.remove("active")),r.style.transform=`translateX(-${l*1230}px)`,n.children[t].classList.add("active")}
+    </li>`,"");c.innerHTML=i}else c.innerHTML="<li></li>",d.textContent="",j.style.margin="0"}const f=document.querySelector(".slider-left-btn"),b=document.querySelector(".slider-right-btn"),r=document.querySelector(".project-img-list"),n=document.querySelector(".project-small-img-list");let e=0;f.addEventListener("click",()=>{e<=0?e=r.children.length-1:e--,g(e)});b.addEventListener("click",()=>{e===r.children.length-1?e=0:e++,g(e)});function g(t){Array.from(n.children).map(l=>l.classList.remove("active")),r.style.transform=`translateX(-${e*1230}px)`,n.children[t].classList.add("active")}
 //# sourceMappingURL=commonHelpers6.js.map
