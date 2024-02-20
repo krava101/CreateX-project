7cd58b2a6d5f23ce06502263c1be3405b600719a
krava101 <141029431+krava101@users.noreply.github.com>
2024-02-20 22:13:01 +0000

    Deploying to gh-pages from @ krava101/CreateX-project@67f5f8692ee0583383a8bc9ec8da23f22a9d69b6 🚀

--- commonHelpers7.js
+++ commonHelpers7.js
@@ -1,4 +1,4 @@
-import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                     */const a=[{id:0,type:"Construction",previewImg:new URL("/CreateX-project/assets/construction-73796e43.jpg",self.location).href,offers:["Interior design of apartments","Interior design of private houses"]},{id:1,type:"Project Development",previewImg:new URL("/CreateX-project/assets/project-development-a7afb9e0.jpg",self.location).href,offers:["Interior design of apartments","Interior design of private houses"]},{id:2,type:"Interior Design",previewImg:new URL("/CreateX-project/assets/interior-design-4386f777.jpg",self.location).href,offers:["Interior design of apartments","Interior design of private houses"]},{id:3,type:"Repairs",previewImg:new URL("/CreateX-project/assets/repairs-a3af7a95.jpg",self.location).href,offers:["Interior design of apartments","Interior design of private houses"]}],c=document.querySelector(".offer"),t=window.location.href,f=t[t.length-1],r=new URL("/CreateX-project/assets/icons-ea889f47.svg",self.location).href;function l(e){const s=a[e],i=`
+import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                     */import{p as c}from"./assets/data-projects-98539dd2.js";const n=[{id:0,type:"Construction",status:"construction",previewImg:new URL("/CreateX-project/assets/construction-73796e43.jpg",self.location).href,offers:["Interior design of apartments","Interior design of private houses"]},{id:1,type:"Project Development",status:"development",previewImg:new URL("/CreateX-project/assets/project-development-a7afb9e0.jpg",self.location).href,offers:["Interior design of apartments","Interior design of private houses"]},{id:2,type:"Interior Design",status:"design",previewImg:new URL("/CreateX-project/assets/interior-design-4386f777.jpg",self.location).href,offers:["Interior design of apartments","Interior design of private houses"]},{id:3,type:"Repairs",status:"repairs",previewImg:new URL("/CreateX-project/assets/repairs-a3af7a95.jpg",self.location).href,offers:["Interior design of apartments","Interior design of private houses"]}],l=document.querySelector(".offer"),f=document.querySelector(".project-list"),o=window.location.href,p=o[o.length-1],i=new URL("/CreateX-project/assets/icons-ea889f47.svg",self.location).href;function u(e){const s=n[e],r=`
     <div class="container">
           <img
             class="offer-img"
@@ -8,12 +8,12 @@ import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                
           <div class="offer-info">
             <h2 class="section-title">We offer</h2>
             <ul class="offer-list list">
-              ${s.offers.reduce((o,n)=>o+`<li>
+              ${s.offers.reduce((t,a)=>t+`<li>
                 <h4 class="offer-list-title">
                   <svg class="offer-list-icon">
-                    <use href=${r+"#icon-plus"}></use>
+                    <use href=${i+"#icon-plus"}></use>
                   </svg>
-                  ${n}
+                  ${a}
                 </h4>
                 <p class="offer-list-about">
                   Adipiscing nunc arcu enim elit mattis eu placerat proin.
@@ -24,5 +24,17 @@ import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                
             </ul>
           </div>
         </div>
-  `;c.innerHTML=i}l(f);const p=document.querySelector(".offer-list");p.addEventListener("click",e=>{e.target.nodeName==="LI"&&(e.target.classList.toggle("visible"),e.target.classList.contains("visible")?e.target.querySelector("use").href.baseVal=r+"#icon-minus":setTimeout(()=>{e.target.querySelector("use").href.baseVal=r+"#icon-plus"},400))});
+  `;l.innerHTML=r,d(s.status)}u(p);const m=document.querySelector(".offer-list");m.addEventListener("click",e=>{e.target.nodeName==="LI"&&(e.target.classList.toggle("visible"),e.target.classList.contains("visible")?e.target.querySelector("use").href.baseVal=i+"#icon-minus":setTimeout(()=>{e.target.querySelector("use").href.baseVal=i+"#icon-plus"},400))});function d(e){const s=c.filter(r=>r.status===e).reduce((r,t)=>r+`
+    <li class="project-card">
+      <img
+        class="project-img"
+        src="${t.previewImg}"
+        alt=""
+      />
+      <div class="project-card-box">
+        <h5 class="card-title">${t.name}</h5>
+        <p class="project-card-text">${t.type}</p>
+        <a class="project-card-link" href="project.html?project=project${t.id}">view project</a>
+      </div>
+    </li>`,"");f.innerHTML=s}
 //# sourceMappingURL=commonHelpers7.js.map
