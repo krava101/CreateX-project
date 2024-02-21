4a400a65928c9c4c014bfb4f5d83c7ade4a56d10
krava101 <141029431+krava101@users.noreply.github.com>
2024-02-21 15:07:57 +0000

    Deploying to gh-pages from @ krava101/CreateX-project@8d1b459a70bda13bf328cd4c54fdd2427c16b945 🚀

--- commonHelpers9.js
+++ commonHelpers9.js
@@ -1,4 +1,4 @@
-import{p as l}from"./assets/data-projects-3a722654.js";/* empty css                     */import"./assets/comments-a49f048a.js";const n=document.querySelector(".projects-list"),m=document.querySelector(".media-list"),p=document.querySelectorAll(".media-btn"),s=document.querySelector(".projects-load-more");let r=1;const o=(e="all",c=1)=>{s.style.display="flex",c*9>=l.length&&(s.style.display="none");let i=l.slice(0,c*9);e!=="all"&&(i=l.filter(a=>a.status===e),s.style.display="none");const d=i.reduce((a,t)=>a+`<li class="projects-card">
+import{p as l}from"./assets/data-projects-3a722654.js";/* empty css                     */import{l as n}from"./assets/comments-a49f048a.js";const m=document.querySelector(".projects-list"),p=document.querySelector(".media-list"),y=document.querySelectorAll(".media-btn"),s=document.querySelector(".projects-load-more");let o=1;const r=(e="all",c=1)=>{s.style.display="flex",c*9>=l.length&&(s.style.display="none");let i=l.slice(0,c*9);e!=="all"&&(i=l.filter(a=>a.status===e),s.style.display="none");const d=i.reduce((a,t)=>a+`<li class="projects-card">
               <img
                 class="projects-img"
                 src="${t.previewImg}"
@@ -11,5 +11,5 @@ import{p as l}from"./assets/data-projects-3a722654.js";/* empty css             
                   >view project</a
                 >
               </div>
-            </li>`,"");n.innerHTML=d};o();m.addEventListener("click",e=>{p.forEach(c=>c.classList.remove("active")),e.target.classList.add("active"),o(e.target.dataset.media)});s.addEventListener("click",e=>{r+=1,o("all",r),r*9>=l.length&&(s.style.display="none")});
+            </li>`,"");m.innerHTML=d};r();n(0);p.addEventListener("click",e=>{y.forEach(c=>c.classList.remove("active")),e.target.classList.add("active"),r(e.target.dataset.media)});s.addEventListener("click",e=>{o+=1,r("all",o),o*9>=l.length&&(s.style.display="none")});
 //# sourceMappingURL=commonHelpers9.js.map
