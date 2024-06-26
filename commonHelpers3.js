007cd08227254349dff261cf74d6a94beeb96407
krava101 <141029431+krava101@users.noreply.github.com>
2024-06-26 12:54:10 +0000

    Deploying to gh-pages from @ krava101/CreateX-project@19c577c9bf5640bd535b547408c996439b9059fb 🚀

--- commonHelpers3.js
+++ commonHelpers3.js
@@ -1,14 +1,2 @@
-import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                     */import{p as i}from"./assets/data-projects-98539dd2.js";import{a as n}from"./assets/similar-projects-ebbc83be.js";import{l}from"./assets/comments-a49f048a.js";import"./assets/main-form-80133312.js";const m=document.querySelector(".question-form"),u=document.querySelector(".hero-steps-list"),p=document.querySelector(".hero-left-btn"),d=document.querySelector(".hero-right-btn"),s=document.querySelector(".hero-pag-list"),h=document.querySelector("#p-list");let r=0;const o={userName:"",phone:"",message:""};function g(){const e=i.reduce((c,t)=>c+`
-    <li class="project-card">
-      <img
-        class="project-img"
-        src="${t.previewImg}"
-        alt=""
-      />
-      <div class="project-card-box">
-        <h5 class="card-title">${t.name}</h5>
-        <p class="project-card-text">${t.type}</p>
-        <a class="project-card-link" href="project.html?project=project${t.id}">view project</a>
-      </div>
-    </li>`,"");h.innerHTML=e,n()}g();l(0);m.addEventListener("submit",e=>{e.preventDefault(),o.userName=e.currentTarget.user_name.value,o.phone=e.currentTarget.user_tel.value,o.message=e.currentTarget.user_message.value,console.log(o),e.currentTarget.reset()});p.addEventListener("click",e=>{r<=0?r=0:r--,a(r)});d.addEventListener("click",e=>{r===3||r++,a(r)});function a(e){Array.from(s.children).map(c=>c.classList.remove("active-hero-pag")),u.style.transform=`translateX(${-e*1230}px)`,s.children[e].classList.add("active-hero-pag")}
+import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                     */import"./assets/data-projects-98539dd2.js";import"./assets/similar-projects-ebbc83be.js";import"./assets/controls-57926c4e.js";document.querySelector(".question-form");const o=document.querySelector(".hero-steps-list"),i=document.querySelector(".hero-left-btn"),l=document.querySelector(".hero-right-btn"),s=document.querySelector(".hero-pag-list");document.querySelector("#p-list");let e=0,n;function c(t){const r=o.firstElementChild.getBoundingClientRect();s.querySelector(".active-hero-pag").classList.remove("active-hero-pag"),o.style.transform=`translateX(${-t*r.width}px)`,s.children[t].classList.add("active-hero-pag")}i.addEventListener("click",()=>{e<=0?e=3:e--,c(e)});l.addEventListener("click",()=>{e===3?e=0:e++,c(e)});o.addEventListener("touchstart",t=>{n=t.touches[0].clientX});o.addEventListener("touchend",t=>{const r=t.changedTouches[0].clientX;r>n?e<=0?e=3:e--:r<n&&(e===3?e=0:e++),c(e)});o.addEventListener("mouseover",t=>{o.addEventListener("scroll",r=>{console.log("d")})});
 //# sourceMappingURL=commonHelpers3.js.map
