e84f8a542df877a9ab81fa31aeffaab2e5faca47
krava101 <141029431+krava101@users.noreply.github.com>
2024-06-26 20:24:30 +0000

    Deploying to gh-pages from @ krava101/CreateX-project@43e705fe712a84b59ca8159e4f5251484cf23b6e 🚀

--- commonHelpers10.js
+++ commonHelpers10.js
@@ -1,27 +1,15 @@
-import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                     */import{p as n}from"./assets/data-projects-98539dd2.js";import"./assets/main-form-80133312.js";const d=[{id:0,avatar:new URL("/CreateX-project/assets/project-1-68d98b07.jpg",self.location).href,comment:"Ipsum aute sunt aliquip aute et occaecat. Anim minim do cillum eiusmod enim. Consectetur magna cillum consequat minim  laboris cillum laboris voluptate minim proident exercitation ullamco. ",nickname:"Shawn Edwards",position:"HR Manager",companyName:"WiPo"},{id:1,avatar:new URL("/CreateX-project/assets/project-6-5d8ae179.jpg",self.location).href,comment:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, ratione delectus adipisci tenetur a exercitationem cumque vitae molestiae ad, odio sit, dignissimos ipsa iusto debitis cupiditate necessitatibus! Aliquam, voluptates iure?",nickname:"Lo Wung",position:"General Director",companyName:"MSU Group"},{id:2,avatar:new URL("/CreateX-project/assets/project-2-999359c8.jpg",self.location).href,comment:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic, dolorum, minima earum saepe animi eaque numquam sequi nisi repudiandae ea magnam facilis fuga voluptas doloremque possimus? Nobis repellendus commodi illo.",nickname:"Denn Miller",position:"Owner",companyName:""},{id:3,avatar:new URL("/CreateX-project/assets/project-8-130fc28a.jpg",self.location).href,comment:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea dolor, ex mollitia impedit labore sit nam totam officiis odio assumenda, recusandae, deserunt iste cumque! Rem veritatis modi cum dolorem deserunt!",nickname:"Anrew Karlson",position:"Owner",companyName:""}],u=document.querySelector("#comments"),v=document.querySelector("#rev-left-btn"),g=document.querySelector("#rev-right-btn");let i=0;function m(t){const e=d[t],o=`
-        <h2 class="section-title">What our clients are saying</h2>
-        <img
-          src="${e.avatar}"
-          alt="Client's avatar"
-          class="reviews-avatar"
-        />
-        <p class="reviews-text">
-          ${e.comment}
-        </p>
-        <p class="reviews-nickname">${e.nickname}</p>
-        <p class="reviews-position">${e.position+(e.companyName!==""?", "+e.companyName:"")}</p>
-    `;u.innerHTML=o}g.addEventListener("click",t=>{i===d.length-1?i=0:i++,m(i)});v.addEventListener("click",t=>{i>=1&&i--,m(i)});const y=document.querySelector(".projects-list"),f=document.querySelector(".media-list"),j=document.querySelectorAll(".media-btn"),s=document.querySelector(".projects-load-more");let r=1;const l=(t="all",e=1)=>{s.style.display="flex",e*9>=n.length&&(s.style.display="none");let o=n.slice(0,e*9);t!=="all"&&(o=n.filter(c=>c.status===t),s.style.display="none");const p=o.reduce((c,a)=>c+`<li class="projects-card">
+import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                     */import{p as l}from"./assets/data-projects-98539dd2.js";import{l as n}from"./assets/comments-a49f048a.js";import"./assets/main-form-80133312.js";const m=document.querySelector(".projects-list"),p=document.querySelector(".media-list"),y=document.querySelectorAll(".media-btn"),s=document.querySelector(".projects-load-more");let o=1;const r=(e="all",c=1)=>{s.style.display="flex",c*9>=l.length&&(s.style.display="none");let i=l.slice(0,c*9);e!=="all"&&(i=l.filter(a=>a.status===e),s.style.display="none");const d=i.reduce((a,t)=>a+`<li class="projects-card">
               <img
                 class="projects-img"
-                src="${a.previewImg}"
-                alt="${a.name}"
+                src="${t.previewImg}"
+                alt="${t.name}"
               />
               <div class="projects-card-box">
-                <h5 class="card-title">${a.name}</h5>
-                <p class="projects-card-text">${a.type}</p>
-                <a class="projects-card-link" href="./project.html?project=${a.id}"
+                <h5 class="card-title">${t.name}</h5>
+                <p class="projects-card-text">${t.type}</p>
+                <a class="projects-card-link" href="./project.html?project=${t.id}"
                   >view project</a
                 >
               </div>
-            </li>`,"");y.innerHTML=p};l();m(0);f.addEventListener("click",t=>{j.forEach(e=>e.classList.remove("active")),t.target.classList.add("active"),l(t.target.dataset.media)});s.addEventListener("click",t=>{r+=1,l("all",r),r*9>=n.length&&(s.style.display="none")});
+            </li>`,"");m.innerHTML=d};r();n(0);p.addEventListener("click",e=>{y.forEach(c=>c.classList.remove("active")),e.target.classList.add("active"),r(e.target.dataset.media)});s.addEventListener("click",e=>{o+=1,r("all",o),o*9>=l.length&&(s.style.display="none")});
 //# sourceMappingURL=commonHelpers10.js.map
--- commonHelpers3.js
+++ commonHelpers3.js
@@ -1,2 +1,14 @@
-import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                     */import"./assets/data-projects-98539dd2.js";import"./assets/similar-projects-ebbc83be.js";import"./assets/controls-57926c4e.js";document.querySelector(".question-form");const n=document.querySelector(".hero-steps-list"),s=document.querySelector(".hero-left-btn"),l=document.querySelector(".hero-right-btn"),i=document.querySelector(".hero-pag-list");document.querySelector("#p-list");let e=0,r;function c(t){const o=n.firstElementChild.getBoundingClientRect();i.querySelector(".active-hero-pag").classList.remove("active-hero-pag"),n.style.transform=`translateX(${-t*o.width}px)`,i.children[t].classList.add("active-hero-pag")}s.addEventListener("click",()=>{e<=0?e=3:e--,c(e)});l.addEventListener("click",()=>{e===3?e=0:e++,c(e)});n.addEventListener("touchstart",t=>{r=t.touches[0].clientX});n.addEventListener("touchend",t=>{const o=t.changedTouches[0].clientX;o>r?o-r>70&&(e<=0?e=3:e--):o<r&&r-o>70&&(e===3?e=0:e++),c(e)});
+import"./assets/modulepreload-polyfill-ec808ebb.js";import{p as l}from"./assets/data-projects-98539dd2.js";import{a as u}from"./assets/similar-projects-ebbc83be.js";import{l as d}from"./assets/comments-a49f048a.js";import"./assets/controls-57926c4e.js";const m=document.querySelector(".question-form"),a=document.querySelector(".hero-steps-list"),p=document.querySelector(".hero-left-btn"),h=document.querySelector(".hero-right-btn"),n=document.querySelector(".hero-pag-list"),g=document.querySelector("#p-list");let e=0,o;const s={userName:"",phone:"",message:""};function f(){const t=l.reduce((r,c)=>r+`
+    <li class="project-card">
+      <img
+        class="project-img"
+        src="${c.previewImg}"
+        alt=""
+      />
+      <div class="project-card-box">
+        <h5 class="card-title">${c.name}</h5>
+        <p class="project-card-text">${c.type}</p>
+        <a class="project-card-link" href="project.html?project=project${c.id}">view project</a>
+      </div>
+    </li>`,"");g.innerHTML=t,u()}f();d(0);m.addEventListener("submit",t=>{t.preventDefault(),s.userName=t.currentTarget.user_name.value,s.phone=t.currentTarget.user_tel.value,s.message=t.currentTarget.user_message.value,console.log(s),t.currentTarget.reset()});function i(t){const r=a.firstElementChild.getBoundingClientRect();n.querySelector(".active-hero-pag").classList.remove("active-hero-pag"),a.style.transform=`translateX(${-t*r.width}px)`,n.children[t].classList.add("active-hero-pag")}p.addEventListener("click",()=>{e<=0?e=3:e--,i(e)});h.addEventListener("click",()=>{e===3?e=0:e++,i(e)});a.addEventListener("touchstart",t=>{o=t.touches[0].clientX});a.addEventListener("touchend",t=>{const r=t.changedTouches[0].clientX;r>o?r-o>70&&(e<=0?e=3:e--):r<o&&o-r>70&&(e===3?e=0:e++),i(e)});
 //# sourceMappingURL=commonHelpers3.js.map
