43e705fe712a84b59ca8159e4f5251484cf23b6e
krava <kravalex2004@gmail.com>
2024-06-26 23:24:10 +0300

    Cleaned home page 1.0

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import { projects } from '../data/data-projects';
-import activeSimilarProj from './similar-projects';
-// import loadComment from "./comments";
-// import * as mainForm from "./main-form";
-import './controls.js';
+import { projects } from './data/data-projects.js';
+import activeSimilarProj from './js/similar-projects.js';
+import loadComment from './js/comments.js';
+// import * as mainForm from './main-form';
+import './js/controls.js';
 
 const questionForm = document.querySelector('.question-form');
 const heroList = document.querySelector('.hero-steps-list');
@@ -19,35 +19,40 @@ const userQuestion = {
   message: '',
 };
 
-// function loadSimilarProjects() {
-//     const markup = projects.reduce((acc, e) => acc + `
-//     <li class="project-card">
-//       <img
-//         class="project-img"
-//         src="${e.previewImg}"
-//         alt=""
-//       />
-//       <div class="project-card-box">
-//         <h5 class="card-title">${e.name}</h5>
-//         <p class="project-card-text">${e.type}</p>
-//         <a class="project-card-link" href="project.html?project=project${e.id}">view project</a>
-//       </div>
-//     </li>`, '');
-//     projectList.innerHTML = markup;
-//     activeSimilarProj();
-// }
+function loadSimilarProjects() {
+  const markup = projects.reduce(
+    (acc, e) =>
+      acc +
+      `
+    <li class="project-card">
+      <img
+        class="project-img"
+        src="${e.previewImg}"
+        alt=""
+      />
+      <div class="project-card-box">
+        <h5 class="card-title">${e.name}</h5>
+        <p class="project-card-text">${e.type}</p>
+        <a class="project-card-link" href="project.html?project=project${e.id}">view project</a>
+      </div>
+    </li>`,
+    ''
+  );
+  projectList.innerHTML = markup;
+  activeSimilarProj();
+}
 
-// loadSimilarProjects();
-// loadComment(0);
+loadSimilarProjects();
+loadComment(0);
 
-// questionForm.addEventListener("submit", event => {
-//   event.preventDefault();
-//   userQuestion.userName = event.currentTarget.user_name.value;
-//   userQuestion.phone = event.currentTarget.user_tel.value;
-//   userQuestion.message = event.currentTarget.user_message.value;
-//   console.log(userQuestion);
-//   event.currentTarget.reset();
-// })
+questionForm.addEventListener('submit', event => {
+  event.preventDefault();
+  userQuestion.userName = event.currentTarget.user_name.value;
+  userQuestion.phone = event.currentTarget.user_tel.value;
+  userQuestion.message = event.currentTarget.user_message.value;
+  console.log(userQuestion);
+  event.currentTarget.reset();
+});
 
 function activHeroSlider(index) {
   const step = heroList.firstElementChild.getBoundingClientRect();
