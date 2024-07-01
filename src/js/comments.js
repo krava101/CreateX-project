70ce3c91ba99c66620a1d66caa77fb6054762d3e
krava <kravalex2004@gmail.com>
2024-07-01 18:41:33 +0300

    Adaptive for Comments  section

--- src/js/comments.js
+++ src/js/comments.js
@@ -1,36 +1,44 @@
-import comments from "../data/data-comments";
+import comments from '../data/data-comments';
 
-const comSec = document.querySelector("#comments");
-const revLeftBtn = document.querySelector("#rev-left-btn");
-const revRightBtn = document.querySelector("#rev-right-btn");
+const comSec = document.querySelector('#comments');
+const revLeftBtn = document.querySelector('#rev-left-btn');
+const revRightBtn = document.querySelector('#rev-right-btn');
 let revIndex = 0;
 
 function loadComment(num) {
-    const currentComment = comments[num];
-    const markup = `
-        <h2 class="section-title">What our clients are saying</h2>
-        <img
-          src="${currentComment.avatar}"
-          alt="Client's avatar"
-          class="reviews-avatar"
-        />
+  const currentComment = comments[num];
+  const markup = `
+        <div class="review-owner">
+          <img
+            src="${currentComment.avatar}"
+            alt="Client's avatar"
+            class="reviews-avatar"
+          />
+          <span>
+            <p class="reviews-nickname">${currentComment.nickname}</p>
+            <p class="reviews-position">${
+              currentComment.position +
+              (currentComment.companyName !== ''
+                ? ', ' + currentComment.companyName
+                : '')
+            }</p>
+          </span>
+        </div>
         <p class="reviews-text">
           ${currentComment.comment}
         </p>
-        <p class="reviews-nickname">${currentComment.nickname}</p>
-        <p class="reviews-position">${currentComment.position + (currentComment.companyName !== '' ? ", " + currentComment.companyName : "")}</p>
-    `
-    comSec.innerHTML = markup;
+    `;
+  comSec.innerHTML = markup;
 }
 
-revRightBtn.addEventListener("click", event => {
-    revIndex === comments.length-1 ? revIndex = 0 : revIndex++;
-    loadComment(revIndex);
-})
+loadComment(revIndex);
 
-revLeftBtn.addEventListener("click", event => {
-    revIndex >= 1 ? revIndex-- : null;
-    loadComment(revIndex);
-})
+revRightBtn.addEventListener('click', event => {
+  revIndex === comments.length - 1 ? (revIndex = 0) : revIndex++;
+  loadComment(revIndex);
+});
 
-export default loadComment;
+revLeftBtn.addEventListener('click', event => {
+  revIndex >= 1 ? revIndex-- : null;
+  loadComment(revIndex);
+});
