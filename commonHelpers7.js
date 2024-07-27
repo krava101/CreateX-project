1160ff1e1161fde78f8b92bf444cf42671b3cb4f
krava101 <141029431+krava101@users.noreply.github.com>
2024-07-27 12:55:25 +0000

    Deploying to gh-pages from @ krava101/CreateX-project@0fc54fa7f3cbd7f48a05902a092fbde1eb4c88bb 🚀

--- commonHelpers7.js
+++ commonHelpers7.js
@@ -1,69 +1,38 @@
-import"./assets/header-4ffb22f8.js";/* empty css                     */import{p as o}from"./assets/data-projects-98539dd2.js";import{l as n}from"./assets/projects-slider-efec6661.js";const m=document.querySelector(".hero"),g=window.location.href,u=new URL(g),d=+u.searchParams.get("project"),r=new URL("/CreateX-project/assets/icons-97ef5a9b.svg",self.location).href,l=o[d],p=`
-  <div class="container">
-  <p class="page-path">
-    <span class="page-path-dark">Homepage / Work</span> / ${l.name}
-  </p>
-  <h1 class="page-title">${l.name}</h1>
-  <div class="project-img-list-wrapper">
-    <ul class="project-img-list list">
-      <li
-        style="background-image: url(${l.images[0]})"
-      ></li>
-      <li
-        style="background-image: url(${l.images[1]})"
-      ></li>
-      <li
-        style="background-image: url(${l.images[2]})"
-      ></li>
-      <li
-        style="background-image: url(${l.images[3]})"
-      ></li>
-      <li
-        style="background-image: url(${l.images[4]})"
-      ></li>
-      <li
-        style="background-image: url(${l.images[5]})"
-      ></li>
-    </ul>
-    <button class="slider-left-btn" type="button">
-      <svg class="slider-left-icon">
-        <use href=${r+"#icon-left-arrow"}></use>
-      </svg>
-    </button>
-    <button class="slider-right-btn" type="button">
-      <svg class="slider-right-icon">
-        <use href=${r+"#icon-right-arrow"}></use>
-      </svg>
-    </button>
-  </div>
-  <ul class="project-small-img-list list">
-    <li class="project-small-img-item active" style="background-image: url(${l.images[0]})"></li>
-    <li class="project-small-img-item" style="background-image: url(${l.images[1]})"></li>
-    <li class="project-small-img-item" style="background-image: url(${l.images[2]})"></li>
-    <li class="project-small-img-item" style="background-image: url(${l.images[3]})"></li>
-    <li class="project-small-img-item" style="background-image: url(${l.images[4]})"></li>
-    <li class="project-small-img-item" style="background-image: url(${l.images[5]})"></li>
-  </ul>
-  <div class="project-info-box">
-    <div class="project-goal">
-      <h2 class="section-title">Project goal</h2>
-      <p class="project-goal-text">
-        ${l.description}
-      </p>
-    </div>
-    <ul class="project-info-list list">
-      <li>Location</li>
-      <li>${l.location}</li>
-      <li>CLIENT</li>
-      <li>${l.client}</li>
-      <li>ARCHITECT</li>
-      <li>${l.architect}</li>
-      <li>SIZE</li>
-      <li>${l.size}</li>
-      <li>VALUE</li>
-      <li>${l.value}</li>
-      <li>COMPLETED</li>
-      <li>${l.completed}</li>
-    </ul>
-  </div>`;m.innerHTML=p;n(l);const y=document.querySelector(".slider-left-btn"),h=document.querySelector(".slider-right-btn"),t=document.querySelector(".project-img-list"),c=document.querySelector(".project-small-img-list"),a=document.querySelector(".project-small-img-list");let e=0;y.addEventListener("click",()=>{e<=0?e=t.children.length-1:e--,s(e)});h.addEventListener("click",()=>{e===t.children.length-1?e=0:e++,s(e)});a.addEventListener("click",i=>{i.target!==i.currentTarget&&(e=Array.from(a.children).indexOf(i.target),s(e))});function s(i){c.querySelector(".active").classList.remove("active"),t.style.transform=`translateX(-${e*1230}px)`,c.children[i].classList.add("active")}
+import"./assets/header-4ffb22f8.js";/* empty css                     */import{p as o}from"./assets/data-projects-98539dd2.js";import{l as n}from"./assets/projects-slider-efec6661.js";const a=window.location.href,s=new URL(a),m=+s.searchParams.get("project"),e=o[m],g=document.getElementById("project-title"),d=document.getElementById("project-path"),u=document.getElementById("project-gallery"),p=document.getElementById("project-small-gallery"),y=document.getElementById("project-info"),j=document.getElementById("project-description");g.textContent=e.name;d.textContent=e.name;u.innerHTML=`
+  <li
+    style="background-image: url(${e.images[0]})"
+  ></li>
+  <li
+    style="background-image: url(${e.images[1]})"
+  ></li>
+  <li
+    style="background-image: url(${e.images[2]})"
+  ></li>
+  <li
+    style="background-image: url(${e.images[3]})"
+  ></li>
+  <li
+    style="background-image: url(${e.images[4]})"
+  ></li>
+  <li
+    style="background-image: url(${e.images[5]})"
+  ></li>`;p.innerHTML=`
+  <li class="project-small-img-item active" style="background-image: url(${e.images[0]})"></li>
+  <li class="project-small-img-item" style="background-image: url(${e.images[1]})"></li>
+  <li class="project-small-img-item" style="background-image: url(${e.images[2]})"></li>
+  <li class="project-small-img-item" style="background-image: url(${e.images[3]})"></li>
+  <li class="project-small-img-item" style="background-image: url(${e.images[4]})"></li>
+  <li class="project-small-img-item" style="background-image: url(${e.images[5]})"></li>`;y.innerHTML=`
+  <li>Location</li>
+  <li>${e.location}</li>
+  <li>CLIENT</li>
+  <li>${e.client}</li>
+  <li>ARCHITECT</li>
+  <li>${e.architect}</li>
+  <li>SIZE</li>
+  <li>${e.size}</li>
+  <li>VALUE</li>
+  <li>${e.value}</li>
+  <li>COMPLETED</li>
+  <li>${e.completed}</li>`;j.textContent=e.description;n(e);const r=document.getElementById("project-gallery"),i=document.getElementById("project-small-gallery");document.getElementById("project-info");const $=document.querySelector(".slider-left-btn"),E=document.querySelector(".slider-right-btn");let t=0;$.addEventListener("click",()=>{t<=0?t=r.children.length-1:t--,c(t)});E.addEventListener("click",()=>{t===r.children.length-1?t=0:t++,c(t)});i.addEventListener("click",l=>{l.target!==l.currentTarget&&(t=Array.from(i.children).indexOf(l.target),c(t))});function c(l){i.querySelector(".active").classList.remove("active"),r.style.transform=`translateX(-${t*1230}px)`,i.children[l].classList.add("active")}
 //# sourceMappingURL=commonHelpers7.js.map
