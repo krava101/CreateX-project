776a76f96ff7a09e92d8d09510c6f8d70b52c56f
krava101 <141029431+krava101@users.noreply.github.com>
2024-08-01 17:24:30 +0000

    Deploying to gh-pages from @ krava101/CreateX-project@1c4a5d30ab4bab3fc7e3949f7980c2dd24f4aeab 🚀

--- commonHelpers4.js
+++ commonHelpers4.js
@@ -1,28 +1,28 @@
-import"./assets/header-4ffb22f8.js";/* empty css                     */import{n as o}from"./assets/data-news-2a056899.js";const d=document.querySelector("#news-list"),a=document.getElementById("news-filter"),w=new URL("/CreateX-project/assets/icons-97ef5a9b.svg",self.location).href;let i=1,n="all";r(o,n);function r(t,s="all"){let l=0;s==="all"?l=t:l=t.filter(c=>c.type.toLowerCase()===s.toLowerCase());const m=l.slice((i-1)*6,i*6).reduce((c,e)=>c+`
+import"./assets/header-4ffb22f8.js";/* empty css                     */import{n as d}from"./assets/data-news-2a056899.js";const g=document.querySelector("#news-list"),v=document.getElementById("news-filter"),n=document.getElementById("news-filter-current"),c=document.getElementById("news-filter-mobile"),p=new URL("/CreateX-project/assets/icons-97ef5a9b.svg",self.location).href;let a=1,l="all";m(d,l);n.addEventListener("click",f);v.addEventListener("click",e=>{if(e.target.nodeName==="BUTTON"){const s=e.target,i=v.querySelector(".active");i&&i.classList.remove("active"),a=1,l=s.dataset.filter,m(d,l),s.parentNode.classList.add("active")}});function f(){c.classList.add("active"),n.classList.add("active"),c.addEventListener("click",u),n.addEventListener("click",r),document.addEventListener("click",L)}function L(e){!c.contains(e.target)&&e.target!==n&&r()}function r(){c.classList.remove("active"),n.classList.remove("active"),c.removeEventListener("click",u),n.removeEventListener("click",r),n.addEventListener("click",f),document.removeEventListener("click",L)}function u(e){if(e.target.nodeName==="BUTTON"){const s=e.target;n.textContent=s.textContent;const i=c.querySelector(".hidden");i&&i.classList.remove("hidden"),s.parentNode.classList.add("hidden"),a=1,l=s.dataset.filter,m(d,l),r()}}function m(e,s="all"){let i=0;s==="all"?i=e:i=e.filter(o=>o.type.toLowerCase()===s.toLowerCase());const w=i.slice((a-1)*6,a*6).reduce((o,t)=>o+`
       <li class="news__card">
         <div class="news__card-img-wrapper">
           <img
             class="news__card-img"
-            src="${e.img}"
-            alt="${e.title} image"
+            src="${t.img}"
+            alt="${t.title} image"
           />
         </div>
         <div class="news__card-box">
-          <a class="news__card-title card-title" href="./post.html?post=${e.id}">${e.title}</a>
+          <a class="news__card-title card-title" href="./post.html?post=${t.id}">${t.title}</a>
           <ul class="news__date-list list">
-            <li>${e.type}</li>
-            <li>${e.date}</li>
+            <li>${t.type}</li>
+            <li>${t.date}</li>
             <li class="news__comments">
               <svg class="news__comment-icon">
-                <use href="${w}#icon-comments"></use>
+                <use href="${p}#icon-comments"></use>
               </svg>
               <p>
-                ${e.comments.length?e.comments.length+(e.comments.length>1?" comments":" comment"):"No comments"}
+                ${t.comments.length?t.comments.length+(t.comments.length>1?" comments":" comment"):"No comments"}
               </p>
             </li>
           </ul>
           <p class="news__card-text">
-          ${e.previewText}</p>
+          ${t.previewText}</p>
         </div>
-      </li>`,"");d.innerHTML=m}a.addEventListener("click",t=>{if(t.target.nodeName==="BUTTON"){const s=t.target;a.querySelector(".active").classList.remove("active"),i=1,n=s.dataset.filter,r(o,n),s.parentNode.classList.add("active")}});
+      </li>`,"");g.innerHTML=w}
 //# sourceMappingURL=commonHelpers4.js.map
