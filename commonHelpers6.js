ec9d32202590c458285b74620ee1a6eb9fdf1707
krava101 <141029431+krava101@users.noreply.github.com>
2024-08-03 11:43:10 +0000

    Deploying to gh-pages from @ krava101/CreateX-project@60c292845267614adba958e354954aff24545678 🚀

--- commonHelpers6.js
+++ commonHelpers6.js
@@ -1,17 +1,37 @@
-import"./assets/header-4ffb22f8.js";/* empty css                     */import{n as c}from"./assets/data-news-d0acb961.js";const d=window.location.href,u=new URL(d).searchParams.get("post"),o=c.find(t=>t.id===u),g=document.getElementById("post-path"),x=document.getElementById("post-title"),h=document.getElementById("post-type"),f=document.getElementById("post-date"),_=document.getElementById("post-comments-num");x.textContent=o.title;g.textContent=`/ ${o.title}`;h.textContent=o.type;f.textContent=o.date;const n=o.comments.length;_.textContent=n?n>1?`${n} comments`:"1 comment":"No comments";const $=window.location.href,w=new URL($).searchParams.get("post"),p=c.find(t=>t.id===w),r=new URL("/CreateX-project/assets/icons-97ef5a9b.svg",self.location).href,a=document.getElementById("post-img"),i=document.getElementById("post-text"),y=document.querySelector(".post__img-loader");a.src=p.img;a.alt=`${p.title} image`;a.classList.remove("hidden");y.classList.add("hidden");const I=p.post.reduce((t,e)=>{let s="";switch(e.type){case"preview-text":s=`<p class="post__preview-text">${e.text}</p>`;break;case"quote-text":s=`
+import"./assets/header-4ffb22f8.js";/* empty css                     */import{n as m}from"./assets/data-news-d0acb961.js";const x=window.location.href,_=new URL(x).searchParams.get("post"),o=m.find(t=>t.id===_),r=document.getElementById("post-path"),p=document.getElementById("post-title"),d=document.getElementById("post-type"),u=document.getElementById("post-date"),g=document.getElementById("post-comments-num");p.textContent=o.title;p.classList.remove("loader");r.textContent=`/ ${o.title}`;r.classList.remove("loader");d.textContent=o.type;d.classList.remove("loader");u.textContent=o.date;u.classList.remove("loader");const c=o.comments.length;g.textContent=c?c>1?`${c} comments`:"1 comment":"No comments";g.classList.remove("loader");const L=window.location.href,y=new URL(L).searchParams.get("post"),a=m.find(t=>t.id===y),i=new URL("/CreateX-project/assets/icons-97ef5a9b.svg",self.location).href,l=document.getElementById("post-img"),h=document.getElementById("post-text"),w=document.querySelector(".post__img-loader");l.src=a.img;l.alt=`${a.title} image`;l.classList.remove("hidden");w.classList.add("hidden");const I=a.post.reduce((t,e)=>{let s="";switch(e.type){case"preview-text":s=`<p class="post__preview-text">${e.text}</p>`;break;case"quote-text":s=`
         <p class="post__quote">
           <svg class="post__quote-icon">
-            <use href="${r}#icon-braces"></use>
+            <use href="${i}#icon-braces"></use>
           </svg>
           <p class="post__quote-text">${e.text}</p>
         </p>`;break;case"post-list":s=`
         <ul class="post__subtext-list list">
-          ${e.text.reduce((m,l)=>m+`
+          ${e.text.reduce((f,v)=>f+`
           <li class="post__subtext-item">
             <svg class="post__subtext-icon">
-              <use href="${r}#icon-check"></use>
+              <use href="${i}#icon-check"></use>
             </svg>
-            <p>${l}</p>
+            <p>${v}</p>
           </li>`,"")}
-        </ul>`;break;default:s=`<p class="post__main-text">${e.text}</p>`;break}return t+`<li>${s}</li>`},"");i.innerHTML=I;i.classList.remove("loader");const b=window.location.href,v=new URL(b).searchParams.get("post");c.find(t=>t.id===v);new URL("/CreateX-project/assets/icons-97ef5a9b.svg",self.location).href;document.querySelector(".comments");
+        </ul>`;break;default:s=`<p class="post__main-text">${e.text}</p>`;break}return t+`<li>${s}</li>`},"");h.innerHTML=I;h.classList.remove("loader");const k=window.location.href,b=new URL(k).searchParams.get("post"),n=m.find(t=>t.id===b),C=new URL("/CreateX-project/assets/icons-97ef5a9b.svg",self.location).href,$=document.querySelector(".comments");n.comments.length===0&&$.classList.add("hidden");const B=`
+    <div class="container">
+      <p class="comments-title">${n.comments.length?n.comments.length:"No"} comments</p>
+      <ul class="comments-list list">
+        ${n.comments.reverse().reduce((t,e)=>t+`
+            <li>
+            <div class="comments-info">
+              <p class="comments-nickname">${e.nickname}</p>
+              <p class="comments-date">${e.date}</p>
+            </div>
+            <p class="comment">
+              ${e.comment}
+              <a href="" class="comment-reply">
+                <svg>
+                  <use href="${C}#icon-reply"></use>
+                </svg> Reply
+              </a>
+            </p>
+          </li>`,"")}
+      </ul>
+    </div>`;$.innerHTML=B;
 //# sourceMappingURL=commonHelpers6.js.map
