f7c3d36eeeeea1e4618a297037831a825a051e10
krava <kravalex2004@gmail.com>
2024-08-05 10:35:00 +0300

    Positions page responsive
    Fixed event handlers for modals

--- src/js/pages/positions.js
+++ src/js/pages/positions.js
@@ -1,75 +1,62 @@
-const subBtn = document.querySelector("#sub-btn");
-const cvBtn = document.querySelector("#cv-btn");
-const subModal = document.querySelector(".sub-modal");
-const cvModal = document.querySelector(".cv-modal");
-const location = document.querySelector(".location-select-label");
-const subForm = subModal.querySelector("form");
-const cvForm = cvModal.querySelector("form");
+const subBtn = document.getElementById('sub-btn');
+const cvBtn = document.getElementById('cv-btn');
+const subModal = document.getElementById('sub-modal');
+const cvModal = document.getElementById('cv-modal');
 
-const userSubInfo = {
-    name: '',
-    email: ''
+const location = document.getElementById('location-label');
+
+subBtn.addEventListener('click', () => openModal(subModal, subBtn));
+cvBtn.addEventListener('click', () => openModal(cvModal, cvBtn));
+
+function openModal(modal, openBtn) {
+  modal.classList.add('active');
+  const closeBtn = modal.querySelector('.modal-close-btn');
+
+  const handleCloseModal = () =>
+    closeModal(modal, handleCloseModal, handleClickOutside);
+  const handleClickOutside = event =>
+    clickOutsideTheModal(
+      event,
+      openBtn,
+      modal,
+      handleCloseModal,
+      handleClickOutside
+    );
+
+  closeBtn.addEventListener('click', handleCloseModal);
+  document.addEventListener('click', handleClickOutside);
 }
 
-const userCVInfo = {
-    name: '',
-    location: '',
-    phone: '',
-    email: '',
-    message: '',
-    cv: ''
+function clickOutsideTheModal(
+  event,
+  openBtn,
+  modal,
+  handleCloseModal,
+  handleClickOutside
+) {
+  if (!modal.contains(event.target) && event.target !== openBtn) {
+    closeModal(modal, handleCloseModal, handleClickOutside);
+  }
 }
 
-subBtn.addEventListener("click", () => {
-    subModal.classList.add("active");
-    subModal.querySelector('.modal-close-btn').addEventListener(
-        "click", () => {
-            subModal.classList.remove("active");
-        }
-    )
-})
+function closeModal(modal, handleCloseModal, handleClickOutside) {
+  modal.classList.remove('active');
+  const closeBtn = modal.querySelector('.modal-close-btn');
 
-cvBtn.addEventListener("click", () => {
-    cvModal.classList.add("active");
-    cvModal.querySelector('.modal-close-btn').addEventListener(
-        "click", () => {
-            cvModal.classList.remove("active");
-        }
-    )
-})
+  closeBtn.removeEventListener('click', handleCloseModal);
+  document.removeEventListener('click', handleClickOutside);
+}
 
-location.addEventListener("click", event => {
-    const list = event.currentTarget.querySelector("ul");
-    const input = event.currentTarget.querySelector("input");
-    list.classList.add("active");
-    list.style.transform = "translateY(0%)";
-    if (event.target.nodeName === 'LI') {
-        input.value = event.target.textContent;
-        list.style.transform = "translateY(-150%)";
-        setTimeout(() => {
-            list.classList.remove("active");
-        }, 800)
-    }
-})
-
-subForm.addEventListener("submit", event => {
-    event.preventDefault();
-    userSubInfo.name = event.currentTarget.elements.username.value;
-    userSubInfo.email = event.currentTarget.elements.userEmail.value;
-    console.log(userSubInfo);
-    event.currentTarget.reset();
-}) 
-
-cvForm.addEventListener("submit", event => {
-    event.preventDefault();
-    console.log(event.currentTarget.elements.userLocation.value);
-    userCVInfo.name = event.currentTarget.elements.username.value;
-    userCVInfo.location = event.currentTarget.elements.userLocation.value;
-    userCVInfo.email = event.currentTarget.elements.userEmail.value;
-    userCVInfo.phone = event.currentTarget.elements.userPhone.value;
-    userCVInfo.message = event.currentTarget.elements.userLetter.value;
-    userCVInfo.cv = event.currentTarget.elements.userCV.value;
-    console.log(userCVInfo);
-}) 
-
-
+location.addEventListener('click', event => {
+  const list = event.currentTarget.querySelector('ul');
+  const input = event.currentTarget.querySelector('input');
+  list.classList.add('active');
+  list.style.transform = 'translateY(0%)';
+  if (event.target.nodeName === 'LI') {
+    input.value = event.target.textContent;
+    list.style.transform = 'translateY(-150%)';
+    setTimeout(() => {
+      list.classList.remove('active');
+    }, 800);
+  }
+});
--- src/js/pages/post.js
+++ src/js/pages/post.js
@@ -1,21 +1,3 @@
 import '../post-hero.js';
 import '../post-content.js';
 import '../post-comments.js';
-
-// const newComment = {
-//   id: '',
-//   nickname: '',
-//   date: '',
-//   comment: '',
-// };
-
-// commentForm.addEventListener('submit', event => {
-//   event.preventDefault();
-//   newComment.id = Date.now();
-//   newComment.nickname = event.currentTarget.elements.username.value;
-//   newComment.date = dateFormater(new Date());
-//   newComment.comment = event.currentTarget.elements.userComment.value;
-//   commentForm.reset();
-//   currentPost.comments.push(newComment);
-//   loadComments();
-// });
