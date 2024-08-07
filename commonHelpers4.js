4ba8c944ccebd0a3ad2584fcd2a4e101532e1341
krava101 <141029431+krava101@users.noreply.github.com>
2024-08-07 14:30:42 +0000

    Deploying to gh-pages from @ krava101/CreateX-project@581a0ad120f81c8e5071b30d58bb95f1da4c7a8a 🚀

--- commonHelpers4.js
+++ commonHelpers4.js
@@ -1,28 +1,28 @@
-import"./assets/header-4ffb22f8.js";/* empty css                     */import{n as d}from"./assets/data-news-d0acb961.js";const g=document.querySelector("#news-list"),v=document.getElementById("news-filter"),n=document.getElementById("news-filter-current"),c=document.getElementById("news-filter-mobile"),p=new URL("/CreateX-project/assets/icons-97ef5a9b.svg",self.location).href;let a=1,l="all";m(d,l);n.addEventListener("click",f);v.addEventListener("click",e=>{if(e.target.nodeName==="BUTTON"){const s=e.target,i=v.querySelector(".active");i&&i.classList.remove("active"),a=1,l=s.dataset.filter,m(d,l),s.parentNode.classList.add("active")}});function f(){c.classList.add("active"),n.classList.add("active"),c.addEventListener("click",u),n.addEventListener("click",r),document.addEventListener("click",L)}function L(e){!c.contains(e.target)&&e.target!==n&&r()}function r(){c.classList.remove("active"),n.classList.remove("active"),c.removeEventListener("click",u),n.removeEventListener("click",r),n.addEventListener("click",f),document.removeEventListener("click",L)}function u(e){if(e.target.nodeName==="BUTTON"){const s=e.target;n.textContent=s.textContent;const i=c.querySelector(".hidden");i&&i.classList.remove("hidden"),s.parentNode.classList.add("hidden"),a=1,l=s.dataset.filter,m(d,l),r()}}function m(e,s="all"){let i=0;s==="all"?i=e:i=e.filter(o=>o.type.toLowerCase()===s.toLowerCase());const w=i.slice((a-1)*6,a*6).reduce((o,t)=>o+`
+import"./assets/header-4ffb22f8.js";/* empty css                     */import{n as r}from"./assets/data-news-d0acb961.js";import{C as m}from"./assets/custom-dropdown-fcd182db.js";const w=document.getElementById("news-list"),d=document.getElementById("news-filter"),f=document.getElementById("news-filter-mobile"),p=new URL("/CreateX-project/assets/icons-97ef5a9b.svg",self.location).href;let c=1,i="all";a(r,i);new m("news-filter-current","news-filter-mobile",_);function _(n,t){c=1,i=t.dataset.filter;const s=n.querySelector("span");s&&(s.textContent=t.textContent);const l=f.querySelector(".hidden");l&&l.classList.remove("hidden"),t.parentNode.classList.add("hidden"),a(r,i)}d.addEventListener("click",n=>{if(n.target.nodeName==="BUTTON"){const t=n.target,s=d.querySelector(".active");s&&s.classList.remove("active"),c=1,i=t.dataset.filter,a(r,i),t.parentNode.classList.add("active")}});function a(n,t="all"){let s=0;t==="all"?s=n:s=n.filter(o=>o.type.toLowerCase()===t.toLowerCase());const l=s.slice((c-1)*6,c*6).reduce((o,e)=>o+`
       <li class="news__card">
         <div class="news__card-img-wrapper">
           <img
             class="news__card-img"
-            src="${t.img}"
-            alt="${t.title} image"
+            src="${e.img}"
+            alt="${e.title} image"
           />
         </div>
         <div class="news__card-box">
-          <a class="news__card-title card-title" href="./post.html?post=${t.id}">${t.title}</a>
+          <a class="news__card-title card-title" href="./post.html?post=${e.id}">${e.title}</a>
           <ul class="news__date-list list">
-            <li>${t.type}</li>
-            <li>${t.date}</li>
+            <li>${e.type}</li>
+            <li>${e.date}</li>
             <li class="news__comments">
               <svg class="news__comment-icon">
                 <use href="${p}#icon-comments"></use>
               </svg>
               <p>
-                ${t.comments.length?t.comments.length+(t.comments.length>1?" comments":" comment"):"No comments"}
+                ${e.comments.length?e.comments.length+(e.comments.length>1?" comments":" comment"):"No comments"}
               </p>
             </li>
           </ul>
           <p class="news__card-text">
-          ${t.previewText}</p>
+          ${e.previewText}</p>
         </div>
-      </li>`,"");g.innerHTML=w}
+      </li>`,"");w.innerHTML=l}
 //# sourceMappingURL=commonHelpers4.js.map
