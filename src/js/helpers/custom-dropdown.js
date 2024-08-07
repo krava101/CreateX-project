abea6bc7ffbb1915b405b0018ba083c63101bd83
krava <kravalex2004@gmail.com>
2024-08-07 15:58:11 +0300

    Responsive form on Contacts page
    Also added CustomDropdown menu

---
+++ src/js/helpers/custom-dropdown.js
@@ -0,0 +1,44 @@
+export class CustomDropdown {
+  constructor(triggerId, menuId) {
+    this.trigger = document.getElementById(triggerId);
+    this.menu = document.getElementById(menuId);
+
+    this.openDropdown = this.openDropdown.bind(this);
+    this.closeDropdown = this.closeDropdown.bind(this);
+    this.chooseOption = this.chooseOption.bind(this);
+    this.clickOutside = this.clickOutside.bind(this);
+
+    this.trigger.addEventListener('click', this.openDropdown);
+  }
+
+  openDropdown() {
+    this.menu.classList.add('active');
+    this.menu.addEventListener('click', this.chooseOption);
+    document.addEventListener('click', this.clickOutside);
+    this.trigger.removeEventListener('click', this.openDropdown);
+    this.trigger.addEventListener('click', this.closeDropdown);
+  }
+
+  closeDropdown() {
+    this.menu.classList.remove('active');
+    this.menu.removeEventListener('click', this.chooseOption);
+    document.removeEventListener('click', this.clickOutside);
+    this.trigger.removeEventListener('click', this.closeDropdown);
+    this.trigger.addEventListener('click', this.openDropdown);
+  }
+
+  chooseOption(event) {
+    const click = event.target;
+    if (click.nodeName === 'BUTTON') {
+      this.trigger.textContent = click.textContent;
+      this.closeDropdown();
+    }
+  }
+
+  clickOutside(event) {
+    const click = event.target;
+    if (!this.menu.contains(click) && click !== this.trigger) {
+      this.closeDropdown();
+    }
+  }
+}
---
+++ src/js/pages/contacts.js
@@ -0,0 +1,4 @@
+import { CustomDropdown } from '../helpers/custom-dropdown';
+
+const interestedDd = new CustomDropdown('interested-input', 'interested-list');
+const locationDd = new CustomDropdown('location-btn', 'location-list');
