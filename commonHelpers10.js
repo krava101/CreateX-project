032df879046a98936e917168a85a1c655776ef17
krava101 <141029431+krava101@users.noreply.github.com>
2024-08-08 15:34:29 +0000

    Deploying to gh-pages from @ krava101/CreateX-project@580c9086c652cc2e57c2bf8a0723f432fbc4acc8 🚀

--- commonHelpers10.js
+++ commonHelpers10.js
@@ -1,15 +1,15 @@
-import"./assets/header-4ffb22f8.js";/* empty css                     */import"./assets/comments-accae290.js";import{p as l}from"./assets/data-projects-2fb17aac.js";const m=document.querySelector(".projects-list"),d=document.querySelector(".media-list");document.querySelectorAll(".media-btn");const s=document.querySelector(".projects-load-more");let r=1;const i=(e="all",t=1)=>{s.style.display="flex",t*9>=l.length&&(s.style.display="none");let o=l.slice(0,t*9);e!=="all"&&(o=l.filter(a=>a.status===e),s.style.display="none");const n=o.reduce((a,c)=>a+`<li class="projects-card">
+import"./assets/header-4ffb22f8.js";/* empty css                     */import"./assets/comments-accae290.js";import{p as a}from"./assets/data-projects-2fb17aac.js";const p=document.querySelector(".projects-list"),d=document.querySelector(".media-list"),c=document.querySelector(".projects-load-more");let r=1;const i=(e="all",t=1)=>{c.style.display="flex",t*9>=a.length&&(c.style.display="none");let o=a.slice(0,t*9);e!=="all"&&(o=a.filter(l=>l.status===e),c.style.display="none");const n=o.reduce((l,s)=>l+`<li class="projects-card">
         <img
           class="projects-img"
-          src="${c.previewImg}"
-          alt="${c.name}"
+          src="${s.previewImg}"
+          alt="${s.name}"
         />
         <div class="projects-card-box">
-          <h5 class="card-title">${c.name}</h5>
-          <p class="projects-card-text">${c.type}</p>
-          <a class="projects-card-link" href="./project.html?project=${c.id}"
+          <h5 class="card-title">${s.name}</h5>
+          <p class="projects-card-text">${s.type}</p>
+          <a class="projects-card-link" href="./project.html?project=${s.id}"
             >view project</a
           >
         </div>
-      </li>`,"");m.innerHTML=n};i();d.addEventListener("click",e=>{const t=d.querySelector(".active");t&&t.classList.remove("active"),e.target.classList.add("active"),i(e.target.dataset.media)});s.addEventListener("click",e=>{r+=1,i("all",r),r*9>=l.length&&(s.style.display="none")});
+      </li>`,"");p.innerHTML=n};i();d.addEventListener("click",e=>{const t=d.querySelector(".active");t&&t.classList.remove("active"),e.target.classList.add("active"),i(e.target.dataset.media)});c.addEventListener("click",e=>{r+=1,i("all",r),r*9>=a.length&&(c.style.display="none")});
 //# sourceMappingURL=commonHelpers10.js.map
