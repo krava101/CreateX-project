53b95be74dbfb19136efd5ed5702c9dca8b8562b
krava <kravalex2004@gmail.com>
2024-08-08 14:25:50 +0300

    Home reviews loading state

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import './js/home-hero.js';
 import './js/controls.js';
-import './js/main-form.js';
-import './js/projects-slider.js';
+import './js/comments.js';
+import './js/load-last-news.js';
 import { loadProjects } from './js/projects-slider.js';
+import './js/main-form.js';
+
 loadProjects();
