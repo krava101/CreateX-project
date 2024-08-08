8b64bb12396f451ab839b78d6cf0bfd694303f90
krava101 <141029431+krava101@users.noreply.github.com>
2024-08-08 12:47:52 +0000

    Deploying to gh-pages from @ krava101/CreateX-project@eca08657866bb041c84637727df1dec3b8425081 🚀

--- commonHelpers10.js
+++ commonHelpers10.js
@@ -1,15 +1,15 @@
-import"./assets/header-4ffb22f8.js";/* empty css                     */import"./assets/comments-accae290.js";import{p as l}from"./assets/data-projects-2fb17aac.js";const n=document.querySelector(".projects-list"),m=document.querySelector(".media-list"),p=document.querySelectorAll(".media-btn"),s=document.querySelector(".projects-load-more");let r=1;const o=(e="all",c=1)=>{s.style.display="flex",c*9>=l.length&&(s.style.display="none");let i=l.slice(0,c*9);e!=="all"&&(i=l.filter(a=>a.status===e),s.style.display="none");const d=i.reduce((a,t)=>a+`<li class="projects-card">
+import"./assets/header-4ffb22f8.js";/* empty css                     */import"./assets/comments-accae290.js";import{p as l}from"./assets/data-projects-2fb17aac.js";const m=document.querySelector(".projects-list"),d=document.querySelector(".media-list");document.querySelectorAll(".media-btn");const s=document.querySelector(".projects-load-more");let r=1;const i=(e="all",t=1)=>{s.style.display="flex",t*9>=l.length&&(s.style.display="none");let o=l.slice(0,t*9);e!=="all"&&(o=l.filter(a=>a.status===e),s.style.display="none");const n=o.reduce((a,c)=>a+`<li class="projects-card">
         <img
           class="projects-img"
-          src="${t.previewImg}"
-          alt="${t.name}"
+          src="${c.previewImg}"
+          alt="${c.name}"
         />
         <div class="projects-card-box">
-          <h5 class="card-title">${t.name}</h5>
-          <p class="projects-card-text">${t.type}</p>
-          <a class="projects-card-link" href="./project.html?project=${t.id}"
+          <h5 class="card-title">${c.name}</h5>
+          <p class="projects-card-text">${c.type}</p>
+          <a class="projects-card-link" href="./project.html?project=${c.id}"
             >view project</a
           >
         </div>
-      </li>`,"");n.innerHTML=d};o();m.addEventListener("click",e=>{p.forEach(c=>c.classList.remove("active")),e.target.classList.add("active"),o(e.target.dataset.media)});s.addEventListener("click",e=>{r+=1,o("all",r),r*9>=l.length&&(s.style.display="none")});
+      </li>`,"");m.innerHTML=n};i();d.addEventListener("click",e=>{const t=d.querySelector(".active");t&&t.classList.remove("active"),e.target.classList.add("active"),i(e.target.dataset.media)});s.addEventListener("click",e=>{r+=1,i("all",r),r*9>=l.length&&(s.style.display="none")});
 //# sourceMappingURL=commonHelpers10.js.map
