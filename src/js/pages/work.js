695d1951f9627cb11ded7316c6a3bdf6d9a50659
krava <kravalex2004@gmail.com>
2024-08-28 17:48:24 +0300

    Works page

--- src/js/pages/work.js
+++ src/js/pages/work.js
@@ -1,41 +1,54 @@
-import { projects } from '../../data/data-projects.js';
+import { getProjects } from '../../api/projects';
 
-const projectsList = document.querySelector('.projects-list');
+const projList = document.querySelector('.projects-list');
 const mediaNavList = document.querySelector('.media-list');
 const loadMoreBtn = document.querySelector('.projects-load-more');
+const imgUrl = 'https://res.cloudinary.com/dxseifgey/image/upload/v1723796772';
 let page = 1;
+let projects = [];
 
-const loadProjects = (status = 'all', page = 1) => {
+init();
+
+async function init() {
+  projects = await getProjects();
+  loadProjects();
+}
+
+const loadProjects = async (status = 'all', page = 1) => {
+  let projectsList = projects.slice(0, page * 9);
   loadMoreBtn.style.display = 'flex';
-  page * 9 >= projects.length && (loadMoreBtn.style.display = 'none');
-  let newProjects = projects.slice(0, page * 9);
-  status !== 'all' &&
-    ((newProjects = projects.filter(e => e.status === status)),
-    (loadMoreBtn.style.display = 'none'));
 
-  const markup = newProjects.reduce(
-    (acc, proj) =>
+  if (page * 9 >= projects.length) {
+    loadMoreBtn.style.display = 'none';
+  }
+  if (status !== 'all') {
+    projectsList = projects.filter(e => e.status === status);
+    loadMoreBtn.style.display = 'none';
+  }
+
+  const markup = projectsList.reduce(
+    (acc, proj, i) =>
       acc +
       `<li class="projects-card">
         <img
           class="projects-img"
-          src="${proj.previewImg}"
+          src="${imgUrl + proj.images[0].src}"
           alt="${proj.name}"
         />
         <div class="projects-card-box">
           <h5 class="card-title">${proj.name}</h5>
           <p class="projects-card-text">${proj.type}</p>
-          <a class="projects-card-link" href="./project.html?project=${proj.id}"
+          <a class="projects-card-link" href="./project.html?project=${
+            proj._id
+          }"
             >view project</a
           >
         </div>
       </li>`,
     ''
   );
-  projectsList.innerHTML = markup;
+  projList.innerHTML = markup;
 };
-
-loadProjects();
 
 mediaNavList.addEventListener('click', event => {
   const activeMedia = mediaNavList.querySelector('.active');
--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import './js/home-hero.js';
-import './js/controls.js';
 import './js/comments.js';
 import './js/load-last-news.js';
 import { loadProjects } from './js/projects-slider.js';
